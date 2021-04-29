4cf11050cffd1801e1716ad4987a8f505291835c
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-04-29 09:52:44 +0530

    Component Base is completed and CSS is yet to create for components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './Components/Login/Login'
 import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom';
+//Testing Import Removed before pushing to the Repository.
+import Product from "./Components/Store/Products/Product";
 const App = () =>   {
     return (
         <div className = "App" >
-            <Login/>
+            <Product/>
         </div>
     );
 }
--- src/Components/Store/Products/Product/Product.jsx
+++ src/Components/Store/Products/Product/Product.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Card, CardMedia, CardActions, Typography, IconButton, CardContent} from "@material-ui/core";
-import { AddShoppingCart} from '@material-ui/icons';
-
+import {Card, CardMedia, CardActions, Typography, IconButton, CardContent, Icon} from "@material-ui/core";
+import { AddShoppingCart } from '@material-ui/icons';
+import useStyle from './style'
 const Product = ({product }) => {
     //Connect the style here.
-    const classes = '';
+    const classes = useStyle();
     return (
         <Card className={classes.root}>
             <CardMedia className={classes.media}
@@ -13,9 +13,23 @@ const Product = ({product }) => {
 
                        <CardContent>
                            <div className={classes.cardContent}>
-
+                               <Typography variant= "h5" gutterBottom>
+                                   {product.name}
+                               </Typography>
+                               <Typography variant= "h5" gutterBottom>
+                                   {product.price}
+                               </Typography>
                            </div>
+                           <Typography variant= "h2" color= "textSecondary">
+                               {product.description}
+                           </Typography>
                        </CardContent>
+
+                        <CardActions disableSpacing className={classes.cardActions}>
+                            <IconButton aria-label= "Add to Cart">
+                                <AddShoppingCart />
+                            </IconButton>
+                        </CardActions>
         </Card>
     )
 }
