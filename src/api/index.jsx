776444048b2c224f55ade92701ac5145f5847e5c
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-05-01 14:35:10 +0530

    Routing Created Draft Working (Team)

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -4,5 +4,13 @@ const url = "http://localhost:8073/";
 
 //Create API Calling method 
 export const createUser = (user) =>  axios.post(url + "user", user);
-export const getItems = () => axios.get(url + "item");
 
+//Item API Calling Methods.
+export const getItems = () => axios.get(url + "item");
+export const createItem = (item) =>  axios.post(url + "item", item);
+
+
+
+
+//Add Credit Card Payment 
+export const addCreditCardPayment = (item) =>  axios.post(url + "addCreditCardDetails", item);
