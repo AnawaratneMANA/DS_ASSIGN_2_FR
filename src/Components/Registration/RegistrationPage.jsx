d67e23fe8ac7f785b012125fdcbd80ff21ff5252
Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>
2021-05-03 23:41:16 +0530

    login completed

--- src/Components/Registration/RegistrationPage.jsx
+++ src/Components/Registration/RegistrationPage.jsx
@@ -8,14 +8,15 @@ const RegistrationPage = () => {
     const classes = makeStyles();
     return (
         <React.Fragment>
-
             <CssBaseline />
             <Grid container component="main" className={classes.root}>
                 <CssBaseline />
-                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-                    <Grid item xs={false} sm={4} md={7} className={classes.image}> </Grid>
-                    <Registration />
-                </Grid>
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square
+            >
+                <Registration />
+            </Grid>
+            <Grid item xs={false} sm={4} md={7} className={classes.image}> </Grid>
+            
             </Grid>
         </React.Fragment>
     )
