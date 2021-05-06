7674094768bde8196d1c1b99ce612adf0c43e388
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-05-06 23:43:01 +0530

    Navigation bar updated with Team (Team)

--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography} from "@material-ui/core";
+import {AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography} from "@material-ui/core";
 import {AccountCircle, ShoppingCart} from '@material-ui/icons';
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 //Use as the Logo. (Temp)
 import ShopIcon from '../Store/Icons/shop-icon.png';
 import useStyles from './styles';
@@ -9,47 +9,59 @@ import useStyles from './styles';
 const Navbar = () => {
     const classes = useStyles();
     return (<React.Fragment>
-        <AppBar position ="fixed" className={classes.appBar} style={{ background: '#2d2d2d'}}>
+        <AppBar position="fixed" className={classes.appBar} style={{background: '#2d2d2d'}}>
             <Toolbar>
-                <Typography variant= "h4" className={classes.title} color= "inherit">
-                    <img src={ShopIcon} alt= "Store" height= "48px" className={classes.image}/>
-                    <Link to="/store">
+                <Typography variant="h4" className={`${classes.title} ${classes.image}`} color="inherit">
+                    <img src={ShopIcon} alt="Store" height="48px" className={classes.image}/>
+                    <Link to="/" className={classes.title}>
                         Store
-                    </Link>
-                </Typography>
-                <Typography size="small" variant="body1" className={classes.title}>
-                    <Link to='/payment' className={classes.title}>
-                        Payment
-                    </Link>
-                </Typography>
-
-                <Typography variant="body1" size="small">
-                    <Link to='/registration' className={classes.title}>
-                        Registration
-                    </Link>
-                </Typography>
-
-                <Typography variant="body1" size={"small"}>
-                    <Link to='/additem' className={classes.title}>
-                        AddItems
                     </Link>
                 </Typography>
 
                 <div className={classes.grow}>
-                    <Typography variant="body1" >
+                    <Typography variant="body1">
                         <Link to='/additem' className={classes.title}>
-                            New Add Items
+                            Dashboard
+                        </Link>
+                    </Typography>
+                </div>
+                <div className={classes.grow}>
+                    <Typography variant="body1">
+                        <Link to='/payment' className={classes.title}>
+                            Payment
+                        </Link>
+                    </Typography>
+                </div>
+                <div className={classes.grow}>
+                    <Typography variant="body1">
+                        <Link to='/registration' className={classes.title}>
+                            Registration
+                        </Link>
+                    </Typography>
+                </div>
+                <div className={classes.grow}>
+                    <Typography variant="body1">
+                        <Link to='/login' className={classes.title}>
+                            Login
+                        </Link>
+                    </Typography>
+                </div>
+
+                <div className={classes.grow}>
+                    <Typography variant="body1">
+                        <Link to='/about' className={classes.title}>
+                            About Us
                         </Link>
                     </Typography>
                 </div>
                 <div className={classes.button}>
                     <IconButton>
-                        <Badge badgeContent={2} color = "secondary">
+                        <Badge badgeContent={2} color="secondary">
                             <ShoppingCart style={{fill: "White"}}/>
                         </Badge>
                     </IconButton>
                     <IconButton>
-                        <Badge color = "secondary">
+                        <Badge color="secondary">
                             <AccountCircle style={{fill: "White"}}/>
                         </Badge>
                     </IconButton>
