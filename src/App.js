cb418ce3c1f96963a7aab44cffee4ed7b673de4e
taneesha97 <68469720+taneesha97@users.noreply.github.com>
2021-05-07 19:54:41 +0530

    update , delete method completed

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import PaymentComponent3 from "./Components/Payment/PaymentComponent3";
 import RegistrationPage from './Components/Registration/RegistrationPage'
 import {ProtectedRoute} from "./Validations/protectedRoute";
 import Test from "./Components/Testing/TestingComponent";
+import UpdateItems from "./Components/AddItems/UpdateItems";
 const App = () => {
     const dispatch = useDispatch();
     //Store Population Method.
@@ -37,7 +38,9 @@ const App = () => {
                     <Route exact path="/registration" component={RegistrationPage}></Route>
                     <Route exact path="/" component={Product}></Route>
                     <Route exact path="/additem" component={AddItems}></Route>
+                    <Route exact path="/itemlist" component={ItemList}></Route>
                     <Route exact path="/payment/update-payment1/:id" component={PaymentComponent3}></Route>
+                    <Route exact path="/itemlist/update-item/:id" component={UpdateItems}></Route>
                     <ProtectedRoute exact path="/test" component={Test}/>
 
                 </Switch>
